0c06030075c4ea2dc89586988a3470030cead287
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-23 01:25:17 +0900

    [LI-22] fix: 비즈니스 도메인과 무관한 시스템/요청 관련 오류만 열거
    - 인증/인가, 요청에서 발생 가능한 오류, 기타 서버 오류 열거
    - Team, JWT 관련 코드는 분리 필요; 담당자 처리 요함

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CommonErrorCode.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CommonErrorCode.java
@@ -7,34 +7,41 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
-    // 4XX : Client Error
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    // 401
-    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "유저 인증에 실패했습니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다. 토큰 재발행을 요청해주세요."),
-    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다. 로그인을 다시 해주세요."),
-    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "인증에 필요한 JWT가 없습니다"),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인한 사용자만 이용할 수 있는 기능입니다."),
-    // 403
-    FORBIDDEN(HttpStatus.FORBIDDEN, "액세스 권한이 없습니다."),
-    // 404
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
+    /**
+     * 4XX : Client Error
+     */
+    // 인증 / 인가
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
 
-    INVALID_LOGIN_INFO(HttpStatus.BAD_REQUEST, "로그인 정보가 유효하지 않습니다."),
+    // 요청 오류
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "요청값이 올바르지 않습니다."),
+    MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "필수 요청값이 누락되었습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 요청 방식입니다."),
+
+//    TODO JWT 관련: JwtErrorCode로 분리
+//    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+//    EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 액세스 토큰입니다."),
+//    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 리프레쉬 토큰입니다."),
+//    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "요청에 토큰이 존재하지 않습니다"),
 
 
-    // ------------------- 성공 -------------------
-    // 5XX : Server Error
-    NOT_IMPLEMENTED(HttpStatus.NOT_IMPLEMENTED, "URI가 구현되어 있지 않습니다."),   // 501
-    BAD_GATEWAY(HttpStatus.BAD_GATEWAY, "서버로부터의 응답이 잘못되었습니다."),       // 502
+    /**
+     * 5XX : Server Error
+     */
+    NOT_IMPLEMENTED(HttpStatus.NOT_IMPLEMENTED, "URI가 구현되어 있지 않습니다."),
+    BAD_GATEWAY(HttpStatus.BAD_GATEWAY, "서버로부터의 응답이 잘못되었습니다."),
     GATEWAY_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "응답 대기 시간이 초과되었습니다."),
 
-    DEFAULT(HttpStatus.INTERNAL_SERVER_ERROR, "회원 가입 오류입니다. 오류가 지속되면 문의하시기 바랍니다.");
+    // 시스템 오류
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 처리 중 오류가 발생했습니다."),
+    IO_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "입출력 처리 중 오류가 발생했습니다."),
+
+    DEFAULT(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다. 담당자에게 문의해주세요.");
 
 
-//    // team
+//    TODO team 관련: TeamErrorCode로 분리
 //    TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "팀을 찾을수 없습니다."),
 //    TEAM_LEADER_NOT_FOUND(HttpStatus.NOT_FOUND, "팀리더를 찾을수 없습니다."),
 //    TEAM_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "팀멤버를 찾을수 없습니다."),
