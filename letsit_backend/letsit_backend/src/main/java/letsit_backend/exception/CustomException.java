6007300ab7e846926265b361471600398d3a4bb7
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-22 20:19:37 +0900

    [LI-22] refactor: 에러 코드 상수의 네이밍을 컨벤션에 맞게 적용
    상수 타입이므로 전체 대문자 카멜케이스를 적용함

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CustomException.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CustomException.java
@@ -1,28 +1,26 @@
 package letsit_backend.exception;
 
-import letsit_backend.exception.ErrorCode;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 public class CustomException extends RuntimeException {
 
-    private final ErrorCode errorCode;
+    private final ErrorCode ERROR_CODE;
 
-    public CustomException(ErrorCode errorCode) {
-        super(errorCode.getMessage()); // 예외 메시지로 ErrorCode의 메시지 사용
-        this.errorCode = errorCode;
+    public CustomException(ErrorCode ERROR_CODE) {
+        super(ERROR_CODE.getMessage()); // 예외 메시지로 ErrorCode의 메시지 사용
+        this.ERROR_CODE = ERROR_CODE;
     }
 
     // private final ErrorCode errorCode;
     public CustomException(String message) {
         super(message);
-        this.errorCode = null;
+        this.ERROR_CODE = null;
     }
 
     public CustomException(String message, Throwable cause) {
         super(message, cause);
-        this.errorCode = null;
+        this.ERROR_CODE = null;
     }
 
 
