6486310d096a471d8385ab1d50081eab7fb3cd3f
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-22 20:13:39 +0900

    [LI-22] style: typo 수정

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CustomException.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CustomException.java
@@ -7,12 +7,13 @@ import lombok.Getter;
 @Getter
 public class CustomException extends RuntimeException {
 
-    private final letsit_backend.exception.ErrorCode errorCode;
+    private final ErrorCode errorCode;
 
     public CustomException(ErrorCode errorCode) {
         super(errorCode.getMessage()); // 예외 메시지로 ErrorCode의 메시지 사용
         this.errorCode = errorCode;
     }
+
     // private final ErrorCode errorCode;
     public CustomException(String message) {
         super(message);
