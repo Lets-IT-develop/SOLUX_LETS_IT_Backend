ab142f5a1fd9771399a4584adb65acf6a02e5a5a
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-23 02:48:29 +0900

    [LI-22] feat: Custom Exception 정의
    - 기본적으로 ErrorCode 구현체를 인자로 받아 객체를 생성하는 게 정상이나,
      구현 담당자가 에러 코드를 포함하지 않은 예외를 발생시킬 경우를 대비하기 위해 디폴트 예외 객체도 정의함
    - 디폴트 에러 코드는 lazy loading을 하기 위해, thread-safe할 수 있도록 lazy initialization holder class idiom 방식으로 구현
    - 추후 조직 구조가 안정화되면 해당 로직은 제외하는 게 더 바람직해보임: 에러 코드 인자 투입을 강제함으로써 불분명한 예외 혹은 코딩 실수 발생을 애초에 차단

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CustomException.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/exception/CustomException.java
@@ -1,28 +1,74 @@
 package letsit_backend.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException {
 
-    private final ErrorCode ERROR_CODE;
+    private final ErrorCode errorCode;
 
-    public CustomException(ErrorCode ERROR_CODE) {
-        super(ERROR_CODE.getMessage()); // 예외 메시지로 ErrorCode의 메시지 사용
-        this.ERROR_CODE = ERROR_CODE;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
     }
 
-    // private final ErrorCode errorCode;
+    public CustomException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * 예외 발생 시 에러 코드를 포함시키지 않을 경우 아래 메서드들로 처리: default 에러 코드로 통합
+     */
+
+    public CustomException() {
+        this.errorCode = getDefaultErrorCode();
+    }
+
     public CustomException(String message) {
         super(message);
-        this.ERROR_CODE = null;
+        this.errorCode = getDefaultErrorCode();
     }
 
     public CustomException(String message, Throwable cause) {
         super(message, cause);
-        this.ERROR_CODE = null;
+        this.errorCode = getDefaultErrorCode();
     }
 
 
+    private static ErrorCode getDefaultErrorCode() {
+        return DefaultErrorCodeHolder.DEFAULT_ERROR_CODE;
+    }
+
+    private static class DefaultErrorCodeHolder {
+        private static final ErrorCode DEFAULT_ERROR_CODE = new ErrorCode() {
+            @Override
+            public String name() {
+                return "SERVER_ERROR";
+            }
+
+            @Override
+            public HttpStatus getStatus() {
+                return HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+
+            @Override
+            public String getMessage() {
+                return "서버 오류입니다. 담당자에게 문의해주세요.";
+            }
+
+            @Override
+            public RuntimeException getDefaultException() {
+                return new CustomException("SERVER_ERROR");
+            }
+
+            @Override
+            public RuntimeException getDefaultException(Throwable cause) {
+                return new CustomException("SERVER_ERROR", cause);
+            }
+        };
+    }
 }
 
