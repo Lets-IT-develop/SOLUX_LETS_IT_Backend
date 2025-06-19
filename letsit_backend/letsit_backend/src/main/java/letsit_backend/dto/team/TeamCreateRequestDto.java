46b2fa6128024cd8ddfa446703c37c76d04fcedd
EunSeo0117 <gmjea023@gmail.com>
2025-06-19 17:04:41 +0900

    [LI-36] refactor : 요청 dto 추가

---
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/dto/team/TeamCreateRequestDto.java
@@ -0,0 +1,12 @@
+package letsit_backend.dto.team;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamCreateRequestDto {
+    private String teamName;
+}
