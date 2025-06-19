49a2558797667e67cb44d4be6e87068dab1a07a5
EunSeo0117 <gmjea023@gmail.com>
2025-06-19 17:04:34 +0900

    [LI-36] refactor : 응답 dto에서 링크 제거, TeamMember관련 dto를 내부에서 static으로 생성

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/dto/team/TeamInfoResponseDto.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/dto/team/TeamInfoResponseDto.java
@@ -2,6 +2,7 @@ package letsit_backend.dto.team;
 
 import letsit_backend.model.TeamMember;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,9 +12,20 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Builder
 public class TeamInfoResponseDto {
     private String teamName;
-    private String notionLink;
-    private String githubLink;
-    private List<TeamMemberLoadInfoDto> teamMemberInfo;
+    private List<TeamMemberInfoDto> teamMemberInfo;
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class TeamMemberInfoDto {
+        private Long userId;
+        private TeamMember.Role role;
+        private String userName;
+        private String position;
+        private String ProfileImageUrl;
+    }
 }
