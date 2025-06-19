ab5259152ddaedd491c7ad7e3fac48aba47b49b7
EunSeo0117 <gmjea023@gmail.com>
2025-06-19 17:03:40 +0900

    [LI-36] refactor : 조회 메서드 추가

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/repository/TeamMemberRepository.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/repository/TeamMemberRepository.java
@@ -1,19 +1,30 @@
 package letsit_backend.repository;
 
+import jakarta.persistence.LockModeType;
 import letsit_backend.model.Member;
 import letsit_backend.model.TeamMember;
 import letsit_backend.model.TeamPost;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
-    List<TeamMember> findAllByTeamId(TeamPost teamPost);
-    Optional<TeamMember> findByTeamIdAndUserId(TeamPost teamPost, Member member);
+    List<TeamMember> findAllByTeamId(TeamPost teamId);
+    // @EntityGraph(attributePaths = {"member", "member.profile"})
+    List<TeamMember> findAllByTeamPost(TeamPost teamPost);
     List<TeamMember> findByTeamId_TeamId(Long teamId);
     List<TeamMember> findAllByUserId(Member member);
-    Optional<TeamMember> findTeamMemberByTeamIdAndTeamMemberRole(TeamPost teamPost, TeamMember.Role role);
+    Optional<TeamMember> findByMemberAndTeamPost(Member member, TeamPost teamPost);
+    boolean existsByMemberAndTeamPost(Member member, TeamPost teamPost);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT tm FROM TeamMember tm WHERE tm.teamId = :teamId")
+    List<TeamMember> findAllByTeamPostWithLock(@Param("teamId") TeamPost teamPost);
 
 
 }
