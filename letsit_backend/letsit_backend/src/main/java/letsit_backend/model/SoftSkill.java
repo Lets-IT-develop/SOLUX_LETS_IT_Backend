866336b5cd869219da3c1d8aa2d4d40dc3070cbd
yezixx <skylow917@daum.net>
2025-06-21 21:42:24 +0900

    [LI-27] feat: 소프트 스킬 섹션 추가

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/model/SoftSkill.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/model/SoftSkill.java
@@ -1,6 +1,9 @@
 package letsit_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import jakarta.persistence.*;
+import letsit_backend.dto.post.KoreanEnum;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -15,6 +18,9 @@ public class SoftSkill {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long softSkillId;
 
+    @Enumerated(EnumType.STRING)
+    private SoftSkillSection section;
+
     @Column(nullable = false)
     private String softSkillName;
 
@@ -22,4 +28,22 @@ public class SoftSkill {
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private List<PostSoftSkill> postSoftSkills = new ArrayList<>();
+
+    @AllArgsConstructor
+    @Getter
+    public enum SoftSkillSection implements KoreanEnum {
+        LEADERSHIP("리더쉽"),
+        CRITICAL_THINKING("사고력 및 문제 해결 능력"),
+        WORK_ATTITUDE("업무 태도"),
+        TEAMWORK("팀워크 및 대인관계"),
+        COLLABORATION_STYLE("협업 스타일");
+
+        @JsonValue
+        private final String korean;
+
+        @JsonCreator
+        public static SoftSkill.SoftSkillSection fromKorean(String korean) {
+            return KoreanEnum.fromKorean(SoftSkill.SoftSkillSection.class, korean);
+        }
+    }
 }
