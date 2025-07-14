ab8d1638ae44f80c38df73aa68317674455af46b
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-15 00:10:36 +0900

    [LI-22] fix: 코드 병합 과정에서 삭제되었던 메서드 복구
    - ApplyService에서 사용하는 findByMemberIn이 병합 과정에서 삭제된 것으로 추정
    => 복구 및 주석 추가

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/repository/ProfileRepository.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/repository/ProfileRepository.java
@@ -5,7 +5,12 @@ import letsit_backend.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Profile findByMember(Member member);
+
+    // Apply에서 사용; SELECT * FROM Profiles WHERE user_id IN (리스트에 있는 멤버 객체들 열거)
+    List<Profile> findByMemberIn(List<Member> members);
 }
